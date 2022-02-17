a079da083213b66e61636345405f3dc55294a3ac
Rachel Bock <rachel.online@gmail.com>
2022-02-17 10:21:31 -0700

    Remove old options and Display new option buttons

--- game.js
+++ game.js
@@ -10,17 +10,36 @@ function startGame(){
 
 function displayPlotPoint (pointNumber) {
   // Using pointNumber, find plotPoint with the id equal to pointNumber.
-  let myPlotPoint = plotPoints.filter(plotPoint => plotPoint.id === pointNumber);
+  let myPlotPoint = plotPoints.filter(plotPoint => plotPoint.id === pointNumber)[0];
+
   console.log(myPlotPoint);
 
   // Display the plotDescription from the plotPoint with id equal to pointNumber.
-  textElement.innerText = myPlotPoint[0].plotDescription;
+  textElement.innerText = myPlotPoint.plotDescription;
 
   // Display the options associated with the plotPoint.
     // Remove the old option buttons.
+  while(optionButtonsElement.firstChild) {
+    optionButtonsElement.removeChild(optionButtonsElement.firstChild);
+  }
     // Create the new option buttons.  
-  
+  myPlotPoint.options.forEach(option => {
+    const button = document.createElement('button');
+    button.innerText = option.text;
+    button.classList.add('btn');
+    button.addEventListener('click', () => optionSelection (option));
+    optionButtonsElement.appendChild(button);
+  });
 
+}
+
+function optionSelection (option){
+  console.log("Selection made.  Option = ", option);
+  if(option.nextPlotPoint <= 0){
+    startGame();
+  } else {
+    displayPlotPoint(option.nextPlotPoint);
+  }
 }
 
 const plotPoints = [
